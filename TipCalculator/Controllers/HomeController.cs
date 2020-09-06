fe12bb591abd2e07751ae3372d710a43d63fc8a4
Jacob Hinkebein <jhinkebein3@my.stlcc.edu>
2020-09-06 15:30:10 -0500

    Add project files.

---
+++ TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        //COMPLETE INSTRUCTIONS IN Models FIRST then return here!!!
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //STEP 3:
+            /*
+            Using the IActionResult Index() method in the HomeController of the Future Value program
+            as an example, add three objects to the ViewBag:
+            Fifteen, Twenty, and TwentyFive, and set all to 0
+            */
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculator calc)
+        {
+            //STEP 4:
+            /*
+            Using the IActionResult Index() overloaded method in the HomeController of the Future Value program
+            as an example, 
+            1. Add an if/else statement testing the ModelState's IsValid property
+            2. If valid, set each of the above ViewBag properties to a call of the calc.CalculateTip method that you added in the model TipCalculator.cs.
+            Remember that you need to pass in a percentage in the form of a value 0 <= percentage <= 1.  Thus, 0.15 for Fifteen, etc.
+            3. For not valid, set each of the ViewBag properties to 0
+            */
+            if (ModelState.IsValid)
+            {
+                ViewBag.Fifteen = calc.CalculateTip(0.15);
+                ViewBag.Twenty = calc.CalculateTip(0.20);
+                ViewBag.TwentyFive = calc.CalculateTip(0.25);
+            }
+            else
+            {
+                ViewBag.Fifteen = 0;
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+            return View(calc);
+        }
+
+        //PROCEED TO Index.cshtml NEXT...
+    }
+}
---
+++ TipCalculator/Models/TipCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class Calculator
+    {
+        [Required(ErrorMessage = "Please enter a price.")]
+        [Range(0, 1000000000, ErrorMessage = "Price must be > 0")]
+        public double? MealCost { get; set; }
+        
+
+        //STEP 2:
+        /*
+        Using the CalculateFutureValue() method from the FutureValueModel class as a guide, 
+        create a method called CalculateTip.
+        1. It should return a double and take a double as an argument.
+        2. The argument should represent the percentage of the tip in 0 <= % <= 1 format.
+        3. If MealCost.HasValue is true, return MealCost.Value * the percentage,
+        otherwise return 0
+        */
+        public double CalculateTip(double tipPercent)
+        {
+            if (MealCost.HasValue)
+            {
+                //if (tipPercent > 1) //allows user to input tip in 1 < % < 100 as well --commented out, user doesnt get to input custom percents
+                //{
+                //    tipPercent /= 100;
+                //}
+                return MealCost.Value * tipPercent;
+
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        //PROCEED TO HomeController.cs NEXT...
+    }
+}
